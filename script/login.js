8eb5cdccc78079a475bd7044001bd3c8df0ee01e
Marco <marcomembrilla@gmail.com>
2025-05-15 12:51:40 +0200

    .

--- script/login.js
+++ script/login.js
@@ -9,3 +9,61 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
+
+const pw = document.getElementById('pw');
+const pw2 = document.getElementById('pw2');
+const msg_pw2 = document.getElementById('mensaje-pw2');
+const msg_pw = document.getElementById('mensaje-contraseña');
+
+
+pw2.addEventListener('input', () => {
+    comparar_contraseñas();
+});
+
+pw.addEventListener('input', () => {
+    comparar_contraseñas();
+    verificar_contraseña();
+});
+
+function comparar_contraseñas()
+{
+    if (pw2.value != pw.value)
+    {
+        msg_pw2.style.opacity = '1';
+        msg_pw2.style.height = '15px';
+        msg_pw2.style.width = '100%';
+    }
+    else
+    {
+        msg_pw2.style.opacity = '0';
+        msg_pw2.style.height = '15px';
+        msg_pw2.style.width = '100%';
+    }
+}
+
+function verificar_contraseña() 
+{
+    if (pw.value.length < 8)
+    {
+        msg_pw.style.opacity = '1';
+        msg_pw.style.height = '15px';
+        msg_pw.style.width = '100%';
+    }
+    else
+    {
+        msg_pw.style.opacity = '0';
+        msg_pw.style.height = '15px';
+        msg_pw.style.width = '100%';
+    }
+}
+
+const form = document.querySelector('form');
+const finish = document.getElementsById('boton_registro');
+
+finish.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (!form.checkValidity())
+    {
+        alert('Faltan campos por rellenar');
+    }
+})
